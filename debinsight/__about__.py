540be19597c35af28ac05ce3dcb8bab7b8c54c00
Oliver Maurhart <dyle71@gmail.com>
2019-06-15 08:30:47 +0200

    adds __about__.py

---
+++ debinsight/__about__.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# ------------------------------------------------------------
+# debinsight/__about__.py
+#
+# information about the debinsight package
+#
+# This file is part of debinsight.
+# See the LICENSE file for the software license.
+# (C) Copyright 2019, Oliver Maurhart, dyle71@gmail.com
+# ------------------------------------------------------------
+
+"""This is the debinsight package."""
+
+__author__ = 'Oliver Maurhart'
+__email__ = '<dyle71@gmail.com>'
+__copyright__ = 'Copyright (C) 2019, Oliver Maurhart'
+__license__ = 'MIT'
+__title__ = 'debinsight'
+__summary__ = """A little tool to gather information of installed files of a Debian
+(or derivate) operating system."""
+__version__ = '0.0.1'
+__uri__ =  'https://github.com/dyle71/debinsight'
+
--- debinsight/__init__.py
+++ debinsight/__init__.py
@@ -4,7 +4,7 @@
 # ------------------------------------------------------------
 # debinsight/__init__.py
 #
-# debinsight package file
+# init debinsight package file
 #
 # This file is part of debinsight.
 # See the LICENSE file for the software license.
@@ -13,9 +13,13 @@
 
 """This is the debinsight package."""
 
-__author__ = 'Oliver Maurhart <dyle71@gmail.com>'
-__copyright__ = 'Copyright (C) 2019, Oliver Maurhart'
-__license__ = 'GPL-3'
-__title__ = 'debinsight'
-__version__ = '0.0.1'
-
+from debinsight.__about__ import (
+    __author__,
+    __copyright__,
+    __email__,
+    __license__,
+    __summary__,
+    __title__,
+    __uri__,
+    __version__
+)
